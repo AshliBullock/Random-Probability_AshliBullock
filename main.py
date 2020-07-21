d81b632d1cf8ac5c1f017dce1d1e04615500f0c9
AshliB <ashlibullock16@gmail.com>
2020-07-21 15:42:26 +0000

    my code

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
+#import random
+
+# 3 different lists 10,100,100000 big filled with numbers 1/6
+
+# get the % of how many times a number occurs
+
+
+listTen = [1,6]
+listHundred = [1,6]
+listHundredthousand = [1/6]
+
 for i in range(10):
-ten.append(random.randit(1,6))
-print(hundred[i])
+  #Add random number to listTen 
+
+for i in range(100):
+  #Add random number to listHundred 
+
+for i in range(100000):
+  #Add Random number to listK   
+ 
